9faa00815db4c4b81a994f16a085095c84d86809
Aman Aggarwal <aman@clickapps.co>
2019-08-14 13:56:14 +0530

    Initial Commit

---
+++ AASlidingTopTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AASlidingTopTabBar
+//
+//  Created by Aman Aggarwal on 14/08/19.
+//  Copyright © 2019 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
