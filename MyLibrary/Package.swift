5554844760223b4dd4ee28beac37b378d7f96ac1
Aman Aggarwal <aggarwalaman1985@gmail.com>
2022-05-08 12:31:15 +0530

    Changed package file

--- MyLibrary/Package.swift
+++ MyLibrary/Package.swift
@@ -4,13 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "AASlidingTopTabBar",
-    platforms: [.iOS(.v12)],
+    name: "MyLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AASlidingTopTabBar",
-            targets: ["AASlidingTopTabBar"]),
+            name: "MyLibrary",
+            targets: ["MyLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AASlidingTopTabBar",
+            name: "MyLibrary",
             dependencies: [],
-            path: "AASlidingTopTabBar/AASlidingTopTabBar"),
-        /*.testTarget(
-            name: "PackageTests",
-            dependencies: ["Package"]),*/
+            //path: "./AASlidingTopTabBar/myPackage",
+            sources: ["AASlidingTabController.swift"]
+        ),
+        
     ]
 )
--- MyLibrary/README.md
+++ MyLibrary/README.md
@@ -1,3 +1,3 @@
-# Package
+# MyLibrary
 
 A description of this package.
