de5529fc2d8d7eceff218c310b9680d3329c5470
Aman Aggarwal <aggarwalaman1985@gmail.com>
2022-05-08 12:41:39 +0530

    Added new package

--- AASlidingTopTabBarPackage/Package.swift
+++ AASlidingTopTabBarPackage/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "AASlidingTopTabBarPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "AASlidingTopTabBarPackage",
+            targets: ["AASlidingTopTabBarPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyLibrary",
-            dependencies: [],
-            //path: "./AASlidingTopTabBar/myPackage",
-            sources: ["AASlidingTabController.swift"]
-        ),
-        
+            name: "AASlidingTopTabBarPackage",
+            dependencies: []),
+        .testTarget(
+            name: "AASlidingTopTabBarPackageTests",
+            dependencies: ["AASlidingTopTabBarPackage"]),
     ]
 )
