6406769b0d16dfbeb89998a941a0872839d834f6
Aman Aggarwal <aggarwalaman1985@gmail.com>
2022-05-09 17:34:01 +0530

    Removing unwanted code

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AASlidingTopTabBar",
-            path: "Source",
-            dependencies: []),
+            path: "Source")
     ],
 )
